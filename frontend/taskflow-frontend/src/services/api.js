8bebd37c7992ed1d5fcbd0895da285552cb7fcdc
Gaurav Kaushik <gauravkaushik882@gmail.com>
2026-07-31 00:03:11 +0530

    feat: configure frontend for production API

--- frontend/taskflow-frontend/src/services/api.js
+++ frontend/taskflow-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+ baseURL: "https://taskflow-backend-ytng.onrender.com/api"
 });
 
 // ================= TOKEN =================
